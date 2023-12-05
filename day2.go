b3d620524c8d0a745f7b220e043288d1723729f1
capy69420 <german.paiva@gmail.com>
2023-12-04 21:43:34 -0300

    day 2 part 2

--- day2.go
+++ day2.go
@@ -28,9 +28,12 @@ func main() {
 
 	baseColors := map[string]int{"red": 12, "green": 13, "blue": 14}
 	idsSum := 0
+	colorsSum := 0
 	// parse each line
 	for scanner.Scan() {
 		line := scanner.Text()
+		// part 2 minimum required colors
+		minColors := map[string]int{"red": 0, "green": 0, "blue": 0}
 		// parse line
 		parts := strings.Split(line, ":")
 		gameId, err := strconv.Atoi(strings.Split(parts[0], " ")[1])
@@ -38,19 +41,19 @@ func main() {
 			fmt.Println("Error parsing game id: ", err)
 			return
 		}
-		fmt.Println("gameId: ", gameId)
 		sets := strings.Split(parts[1], ";")
 
 		validGame := true
+		product := 1 // part 2
 		for _, s := range sets {
 			// after the set is done the colors are put back into the bag
 			colorSet := make(ColorSet)
-			//fmt.Println("sets: ", s)
 			colors := strings.Split(s, ",")
 			for _, item := range colors {
 				pair := strings.Split(item, " ")
 				color := pair[2]
 				val := pair[1]
+				// add the color value to the set
 				if intValue, err := strconv.Atoi(val); err == nil {
 					if existingValue, ok := colorSet[color]; ok {
 						colorSet[color] = existingValue + intValue
@@ -61,22 +64,31 @@ func main() {
 					fmt.Println("Invalid value for color: ", val)
 				}
 			}
-			fmt.Println("colorSet:", colorSet)
 			for color, val := range colorSet {
+				// part 2 get the max color value
+				if minColors[color] < val {
+					minColors[color] = val
+				}
 				if baseColors[color] < val {
 					// not valid id
 					validGame = false
 				}
 			}
 		}
+
+		for _, color := range minColors {
+			product = product * color
+		}
 		if validGame {
 			idsSum += gameId
 		}
+		colorsSum += product
 	}
 
 	if err := scanner.Err(); err != nil {
 		fmt.Println("Error during scanning", err)
 	}
 
-	fmt.Println("id sum:", idsSum)
+	fmt.Println("Part 1, id sum:", idsSum)
+	fmt.Println("Part 2, sum of colors product:", colorsSum)
 }
